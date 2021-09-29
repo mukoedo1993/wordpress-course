0e7db8c82a1331c5883053b04835675a6421fa33
mukoedo1993 <wangzcyuanfang1997@gmail.com>
2021-09-29 03:42:49 -0700

    course 15th updated Parent AND cHILDERN PAGES

--- wp-content/themes/fictional-university-theme/page.php
+++ wp-content/themes/fictional-university-theme/page.php
@@ -16,11 +16,21 @@
     </div>
 
     <div class="container container--narrow page-section">
-      <div class="metabox metabox--position-up metabox--with-home-link">
+    <?php
+    	$theParent = wp_get_post_parent_id(get_the_ID());
+    
+    	if($theParent) //get the id of parent page of current page. If doesn't have, then it will be 0.
+    	{?>
+    		<!--crumbbox will be shown iff it is within a child page.-->
+    	    <div class="metabox metabox--position-up metabox--with-home-link">
         <p>
-          <a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main">Our History</span>
+          <a class="metabox__blog-home-link" href="<?php echo get_permalink($theParent);?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($theParent); ?></a> <span class="metabox__main"><?php echo the_title(); /*the title for pages*/?></span>
         </p>
       </div>
+    		
+    	<?php }
+    ?>
+  
 
 	<!--
       <div class="page-links">
