0e760011feaa11dbe4b784cd7fa74fbb068f2115
mukoedo1993 <wangzcyuanfang1997@gmail.com>
2021-10-12 14:15:53 -0700

    course 71st create your own raw JSON data

--- wp-content/themes/fictional-university-theme/inc/search-route.php
+++ wp-content/themes/fictional-university-theme/inc/search-route.php
@@ -1,11 +1,13 @@
 <?php
 
+# customized search route here.
+# http://fictionaluniversity.local/wp-json/university/v1/search
 add_action('rest_api_init', 'universityRegisterSearch');
 
 function universityRegisterSearch() {
 	register_rest_route('university/v1', 'search', array(
 	'methods' => WP_REST_SERVER::READABLE,//Read data. A WP constant for set
-	'callback' => 'universitySearchResults'
+	'callback' => 'universitySearchResults' //the value here is exactly the return value of function universitySearchResults
 	
 	));	//first argument: namespace you want to use, i.e.: wp; second argument: route, i.e.: posts OR pages;
 	//third argument: an array describes what should happen when sb visits this URL.
@@ -13,6 +15,22 @@ function universityRegisterSearch() {
 
 
 function universitySearchResults() {
-	return 'Congratulations, you created a route';
+	$professors = new WP_query(array(
+	'post_type' => 'professor'
+	));
+	
+	$professorResults = array();
+	
+	while($professors->have_posts()) {
+		$professors->the_post();
+		array_push($professorResults, array(
+		'title' => get_the_title(),
+		'permalink' => get_the_permalink()
+		)); //first argument: destination array; second argument: what we want to add on the first array
+	}
+	
+	return $professorResults;
+	
+	//Here, wordpress will convert this array into a JS array.
 }
 ?>
