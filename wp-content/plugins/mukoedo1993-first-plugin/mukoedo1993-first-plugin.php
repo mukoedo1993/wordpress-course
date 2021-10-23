61f2e8a79ca73c783f7eb43de0a646b82b9a2554
mukoedo1993 <wangzcyuanfang1997@gmail.com>
2021-10-23 01:16:26 -0700

    course 119th actually counting the words, chars and read time

--- wp-content/plugins/mukoedo1993-first-plugin/mukoedo1993-first-plugin.php
+++ wp-content/plugins/mukoedo1993-first-plugin/mukoedo1993-first-plugin.php
@@ -15,6 +15,56 @@ class WordCountAndTimePlugin {
 		add_action('admin_menu', array( $this , 'adminPage'));	//2nd argument must be a unique name for a function that has never been used by any other plugin. i.e.: $this=>adminPage();
 		
 		add_action('admin_init', array( $this, 'settings'));
+		
+		add_filter('the_content', array($this, 'ifWrap')); //filter the content for our post.
+	}
+	
+	function ifWrap ($content) {
+		//We only need to change or filter the content if you are on a single blog post and have at least one of three checked. 
+		if (is_main_query() and is_single() and 
+		(get_option('wcp_wordcount', '1')
+		 or get_option('wcp_charactercount', '1')
+		 or get_option('wcp_readtime', '1')
+		 )) {
+			return $this->createHTML($content);		
+		}
+		return $content;
+	}
+	
+	function createHTML ($content) {
+	
+		$html = '<h3>' . esc_html(get_option('wcp_headline', 'Post Statistics')) . '</h3><p>';
+		//In the extreme case, what if our front-end was hacked? esc_html gives us final layer of protection of our frontend.
+		
+		// get word count once because both wordcount and read time will need it.
+		if (get_option('wcp_wordcount', '1') or get_option('wcp_readtime', '1')) {
+		  $wordCount = str_word_count(strip_tags($content));
+		}
+		
+		if (get_option('wcp_wordcount', '1')) {
+		  $html .= 'This post has ' . $wordCount . ' words.<br>';
+		  
+		}
+		
+		if (get_option('wcp_charactercount', '1')) {
+		  $html .= 'This post has ' . strlen(strip_tags($content)) . ' characters.<br>';
+		  
+		}
+		
+		if (get_option('wcp_charactercount', '1')) {
+		  $html .= 'This post will take about ' . round($wordCount/225) . ' minute'
+		  . (round($wordCount/225) == 1? '': 's')
+		  . ' to read.<br>';
+		  
+		}
+		
+		$html .= '</p>';
+		
+		if (get_option('wcp_location', '0') == '0') {
+			return $html . $content;
+		}
+		
+		return $content . $html;
 	}
 	
 	function settings () {
